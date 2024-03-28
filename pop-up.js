ed0b860b8977e7cba0928896db7acf7ef2e11e41
TCISBnamida <150784654+TCISBnamida@users.noreply.github.com>
2024-03-28 10:27:39 +0100

    Update pop-up.js
    pop up qui spawn quand on paye

--- pop-up.js
+++ pop-up.js
@@ -1,2 +1,7 @@
 //penser a changer le url le name et les params mais je sais pas quoi mettre sah
-window.open(url, name, params);
+document.querySelector('button').addEventListener('click', function() {
+    let url = "thanks.html";
+    let name = "ThanksPopup";
+    let params = "height=200,width=200";
+    window.open(url, name, params);
+});
